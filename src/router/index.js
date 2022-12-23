ad5f48b494f756aa56814c484d7a63dc67e2c3ce
ripper <wangyifani@foxmail.com>
2022-12-23 14:59:01 +0800

    适配uTools插件

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import HomeView from '../views/home/index.vue';
 
 Vue.use(VueRouter);
 
@@ -8,7 +7,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView,
+    component: () => import(/* webpackChunkName: "home" */ '../views/home/index.vue'),
   },
   {
     path: '/about',
@@ -21,8 +20,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  mode: 'hash',
   routes,
 });
 
